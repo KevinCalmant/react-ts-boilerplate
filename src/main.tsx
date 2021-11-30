93a1f8f53e28ecad784783ef120dd7b886319ee2
kcalmant <kevin.calmant@ext.soprasteria.com>
2021-11-30 23:05:57 +0100

    dev: sanitize html

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import '@csstools/normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+import App from './App';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
